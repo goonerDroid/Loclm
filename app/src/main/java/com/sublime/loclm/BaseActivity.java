e29f21b70d49963cde98e23b26c88f473444bdce
goonerdroid <williamj949@gmail.com>
2017-11-21 14:02:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/sublime/loclm/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.sublime.loclm;
+
+import android.Manifest;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+/**
+ * Created by goonerdroid on 21/11/17.
+ */
+
+public abstract class BaseActivity extends FragmentActivity {
+
+
+    @Override public void onStart() {
+        super.onStart();
+
+        new RxPermissions(this)
+                .request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe(granted -> {
+                    if (granted) {
+                        onLocationPermissionGranted();
+                    } else {
+                        Toast.makeText(BaseActivity.this,
+                                "We,need this permission to fetch location updates", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+    }
+
+    protected abstract void onLocationPermissionGranted();
+}
